e799578e698e240c72eda847084f10f6ee24cc03
erikdubois <erik.dubois@gmail.com>
2015-06-18 12:49:12 +0200

    Automatic Backup @ Thu Jun 18 12:49:12 CEST 2015

--- step_4_install_zsh_v1.sh
+++ step_4_install_zsh_v1.sh
@@ -36,11 +36,24 @@
 
 sudo apt-get install zsh git -y
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
-# often you do not get the chance to type your password 
-# retype this line again
-sudo chsh erik -s /bin/zsh 
+# changing the theme to random so you can enjoy tons of themes.
 
+sudo sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"random\"/g' ~/.zshrc
+
+# If above line did not work somehow. This is what you should do to enjoy the many themes.
 # go find the hidden .zshrc file and look for ZSH_THEME="robbyrussell" (CTRL+H to find hidden files)
 # change this to ZSH_THEME="random"
 
-# Reboot to see the change
+
+echo
+echo
+echo
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "Often you do not get the chance to type your password"
+echo "Retype this line again and fill in your own username"
+echo "sudo chsh username -s /bin/zsh"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo
+echo
+echo "Log off or reboot to see the change in your terminal".
+echo "----------------------------------------------------"
